f26c536ab75f0868d6c351ea98e72c2840025ee5
michaelgroezinger <michael.groezinger@newbizdigital.de>
2017-05-17 19:05:12 +0200

    better else-if in understand

--- messages/index.js
+++ messages/index.js
@@ -149,7 +149,9 @@ intents.matches('Understand', [
        
             if (activity.entity == 'share') {
                 session.send('Sharing enables you to easily give others access to a document or folder.')
-            } else if  (activity.entity == 'co-author') {
+            } else if  (activity.entity == 'sharing')  {
+                 session.send('Sharing enables you to easily give others access to a document or folder.')
+            } else if  (activity.entity == 'co-author')  {
                  session.send('With this feature you can jointly edit a document. In the Online Version of Office even in real-time.')
             } else if (activity.entity == 'Version History') {
                 session.send('Whenever a document is stored on OneDrive or SharePoint, the old version is stored in the version history.')
