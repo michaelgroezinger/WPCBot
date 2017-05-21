c6e807a57efebbf0087cd114267487f07a30a906
michaelgroezinger <michael.groezinger@newbizdigital.de>
2017-05-21 16:57:01 +0200

    db

--- messages/index.js
+++ messages/index.js
@@ -227,9 +227,9 @@ bot.dialog('/u_od', [
                 maxRetries: 3,
                 retryPrompt: 'You selected a wrong option! Try again.'
             }) ;
-        /*session.send('before choice setting');
-        var choice = result.response.entity;
-        session.send('after choice');*/
+        session.send('before choice setting');
+       // var choice = result.response.entity;
+        session.send('after choice');
 
         session.send('you selected' + result.response.entity);
 
