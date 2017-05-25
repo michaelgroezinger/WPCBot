91213517b0e7a1ee24b45ca397d5de7263e5026f
michaelgroezinger <michael.groezinger@newbizdigital.de>
2017-05-25 09:00:39 +0200

    typo

--- messages/index.js
+++ messages/index.js
@@ -280,7 +280,7 @@ bot.dialog('/u_share', [
 
     function (session, result, next) {
         var scopelocal = session.dialogData.scope;
-        if (scopelocal.entity = "") {
+        if (scopelocal.entity == "") {
         builder.Prompts.confirm(session, 'Do you want to share externally?');
         next();
         } else {
