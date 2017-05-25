f2e8167d9335b38b7a91810969dd2337a4064580
michaelgroezinger <michael.groezinger@newbizdigital.de>
2017-05-25 09:31:51 +0200

    db

--- messages/index.js
+++ messages/index.js
@@ -280,6 +280,7 @@ bot.dialog('/u_share', [
 
     function (session, result, next) {
         var scopelocal = session.dialogData.scope;
+        session.send('debug "' + scopelocal.entity + '"');
         if (scopelocal.entity != "" ) {
         builder.Prompts.confirm(session, 'Do you want to share externally?');
         next();
