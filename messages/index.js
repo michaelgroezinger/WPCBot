fd30052e93170e28533b45bd1b6378db8609aadf
michaelgroezinger <michael.groezinger@newbizdigital.de>
2017-05-28 18:48:23 +0200

    db

--- messages/index.js
+++ messages/index.js
@@ -138,7 +138,7 @@ intents.matches('Understand', [
        
             if  ((activity.entity == 'share') || (activity.entity == "Sharing")) {
                 //session.send('Sharing enables you to easily give others access to a document or folder.')
-                session.beginDialog('/u_share');
+                session.beginDialog('/u_share', session.dialogData.scope);
             } else if  ((activity.entity == 'co - author') || (activity.entity == 'co - authoring') || (activity.entity == 'joint editing'))  {
                  session.send('With this feature you can jointly edit a document. In the Online Version of Office even in real-time.')
             } else if (activity.entity == 'versioning') {
@@ -272,7 +272,7 @@ bot.dialog('/u_share', [
     
     function (session, args, next) {
        builder.Prompts.confirm(session,'Sharing enables you to easily give others access to a document or folder. <br>Do you want to know more about sharing documents?');
-        
+       session.dialogData.scope = args.scope; 
     },
     function (session, result, next) {
         if (!result.response){
